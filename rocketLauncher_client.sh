570e54a9a73c945e65d2761d16887f72d47bc487
alialkhiro@gmail.com <alialkhiro@gmail.com>
2024-02-23 10:09:39 +0100

    fixed issue with server socket not closing when binding or listenning fails resulting in a leaked fd. changed rocketLauncher and added rocketLauncher client to allow valgrind use on server

--- rocketLauncher_client.sh
+++ rocketLauncher_client.sh
@@ -9,17 +9,11 @@ fi
 port=$1
 password=$2
 
-make re
 
+# Wait for the server to start
 sleep 2
-
-terminator -e "./rocketLauncher_client.sh $port $password; exec bash"
-
-# Launch the IRC server
-valgrind --track-fds=all ./ircserv $port $password
-
-# Waimaket for the server to start
-sleep 1
 
 # Launch two instances of irssi, each in a separate terminal window, connecting to the server
 
+terminator -e "irssi -c localhost -p $port -w $password -n ali; exec bash"
+irssi -c localhost -p $port -w $password -n bland; exec bash
--- sources/cmds/cmdJoin.cpp
+++ sources/cmds/cmdJoin.cpp
@@ -6,7 +6,7 @@
 /*   By: aalkhiro <aalkhiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:55:03 by bfiguet           #+#    #+#             */
-/*   Updated: 2024/02/23 08:22:13 by aalkhiro         ###   ########.fr       */
+/*   Updated: 2024/02/23 10:08:05 by aalkhiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,8 +73,6 @@ int	cmdJoin(Server *server, std::vector<std::string> str, User *user)
 				{
 					std::cout << "add " << user->getNick() << " in this channel" << std::endl;
 					cha->addUser(user);
-					if (cha->isInvited(user))
-						cha->setInviteUser(user, false);
 				}
 				std::vector<User *> listUser = cha->getUsers();
 				for (std::vector<User*>::iterator it = listUser.begin(); it != listUser.end(); it++)
