3109b7a7fbc060cfc1a19beaf96561864fc4625f
alialkhiro@gmail.com <alialkhiro@gmail.com>
2024-02-28 14:08:06 +0100

    fixing merg issues

--- sources/cmds/cmdPrivmsg.cpp
+++ sources/cmds/cmdPrivmsg.cpp
@@ -6,7 +6,7 @@
 /*   By: aalkhiro <aalkhiro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 15:55:30 by bfiguet           #+#    #+#             */
-/*   Updated: 2024/02/28 14:02:43 by aalkhiro         ###   ########.fr       */
+/*   Updated: 2024/02/28 14:07:48 by aalkhiro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,30 +17,22 @@ int	cmdPrivmsg(Server *server, std::vector<std::string> args, User *user){
 	std::string	msg;
 	User* dest = server->findUser(args[1]);
 	Channel	*cha = server->findChannel(args[1]);
-	User* dest = server->findUser(args[1]);
-	Channel	*cha = server->findChannel(args[1]);
 
 	if (args.size() < 3)
 	{
 		user->addMsgToSend(ERR_NEEDMOREPARAMS(args[0]));
 		return 1;
 	}
-	if (args[2][0] == ':')
-		msg = args[2].substr(1);
-	for (size_t i = 3; i < args.size(); i++)
-	{
-		msg += " ";
-		msg += args[i];
-	}
+	msg = args[2].substr(1);
+	msg += joinArgs(3, args, ' ');
 	if (args[1][0] != '#' && args[1][0] != '&') //tagret == user
 	{
-		if (dest)
-			dest->addMsgToSend(MSG(user->getNick(), user->getUser(), user->getHost(), args[0], dest->getNick(), msg));
-		else
+		if (!dest)
 		{
 			user->addMsgToSend(ERR_NOSUCHNICK(args[1]));
 			return 1;
 		}
+		dest->addMsgToSend(MSG(user->getNick(), user->getUser(), user->getHost(), args[0], dest->getNick(), msg));
 	}
 	else // target == channel
 	{
